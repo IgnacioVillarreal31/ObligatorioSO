b56ca4af92421c7044df9776f1728299b9bdfe8a
IgnacioVillarreal31 <110312930+IgnacioVillarreal31@users.noreply.github.com>
2023-05-30 21:00:40 -0300

    Add files via upload

--- main.sh
+++ main.sh
@@ -3,11 +3,14 @@
 ayer=`date +%Y-%m-%d -d "yesterday"`
 semana=`date -d "$ayer" +%W`
 archivo=`mktemp`
+archivo2=`mktemp`
+archivo3=`mktemp`
+
+mkdir "`date +%W`" 2>/dev/null
 mkdir "`date +%W`/`date +%Y-%m-%d`" 2>/dev/null
 
 for user in `bash usuariosValidos.sh /etc/passwd`
 do
-
 usuario=`echo $user | cut -d: -f1`
 touch "`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
 
@@ -15,8 +18,27 @@ directorio=`echo $user | cut -d: -f3`
 find $directorio > $archivo
   
 if test -f "$semana/$ayer/$usuario.txt"; then
-bash diferenciaArchivos.sh `grep -v ^- "$semana/$ayer/$usuario.txt"|cut -c2-` $archivo > "`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
-else 
-sed -e 's/^/+/' archivo > "`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
+
+grep -v ^- "$semana/$ayer/$usuario.txt"|cut -c2- > $archivo2
+bash diferenciaArchivos.sh $archivo2 $archivo > $archivo3
+
+if [ -s $archivo3 ]; then
+cat $archivo3 > "`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
+
+else
+sed -e 's/^/ /' $archivo2 > "`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
 fi
+
+else 
+sed -e 's/^/+/' $archivo > "`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
+fi
+
+echo "$directorio"
+echo "archivo 1"
+cat "$archivo"
+echo "archivo 2"
+cat "$archivo2"
+echo "archivo 3"
+cat "$archivo3"
+
 done
