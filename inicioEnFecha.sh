9d3cbfcb24b239a5102aa482a5ca745c0a660dbc
IgnacioVillarreal31 <110312930+IgnacioVillarreal31@users.noreply.github.com>
2023-06-07 18:05:01 -0300

    Add files via upload

--- inicioEnFecha.sh
+++ inicioEnFecha.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-grep "$1""$2"|egrep 'session opened|password check failed|incorrect password'
+
+if [ `echo $2 | cut -c1` -eq "0" ]; then
+grep "$1  `echo $2 | cut -c2`" $3 |egrep 'session opened|password check failed|incorrect password' 
+else
+grep "$1  $2" $3 |egrep 'session opened|password check failed|incorrect 
+fi
--- main.sh
+++ main.sh
@@ -1,52 +1,38 @@
 #!/bin/bash
 
+ruta="/home/ivillarreal/Escritorio/ObligatorioSO-main"
+
 ayer=`date +%Y-%m-%d -d "yesterday"`
 semana=`date -d "$ayer" +%W`
 
-mkdir "`date +%W`" 2>/dev/null
-mkdir "`date +%W`/`date +%Y-%m-%d`" 2>/dev/null
+mkdir $ruta/"`date +%W`" 2>/dev/null
+mkdir $ruta/"`date +%W`/`date +%Y-%m-%d`" 2>/dev/null
+bash $ruta/inicioEnFecha.sh `date -d $ayer +%m` `date -d $ayer +%d`
 
-for user in `bash usuariosValidos.sh`
+for user in `bash $ruta/usuariosValidos.sh`
 do
-archivo=`mktemp`
-archivo2=`mktemp`
-archivo3=`mktemp`
+archivoHoy=`mktemp`
+archivoAyer=`mktemp`
+archivoDiferencia=`mktemp`
 archivoCut=`mktemp`
 
 usuario=`echo $user | cut -d: -f1`
-touch "`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
+touch "$ruta/`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
 
 directorio=`echo $user | cut -d: -f3`
-find $directorio > $archivo
+find $directorio > $archivoHoy
   
-if test -f "$semana/$ayer/$usuario.txt"; then
-	echo "ARCHIVO ANTES:"
-	cat "$semana/$ayer/$usuario.txt"
-	grep -v '^-' "$semana/$ayer/$usuario.txt" > $archivo2
+if test -f "$ruta/$semana/$ayer/$usuario.txt"; then
+	grep -v '^-' "$ruta/$semana/$ayer/$usuario.txt" > $archivoAyer
+	cut -c2- $archivoAyer > $archivoCut
+	bash "$ruta/diferenciaArchivos.sh" $archivoCut $archivoHoy > $archivoDiferencia
 
-	echo "ARCHIVO DESPUES:"
-	#sed -e 's/\s\+//g' $archivo2
-
-	cut -c2- $archivo2 > $archivoCut
-	cat $archivoCut
-	bash diferenciaArchivos.sh $archivoCut $archivo > $archivo3
-
-	if [ -s $archivo3 ]; then
-		cat $archivo3 > "`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
-
+	if [ -s $archivoDiferencia ]; then
+		cat $archivoDiferencia > "$ruta/`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
 	else
-		sed -e 's/^/ /' $archivo > "`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
+		sed -e 's/^/ /' $archivoHoy > "$ruta/`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
 	fi
 else 
-	sed -e 's/^/+/' $archivo > "`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
+	sed -e 's/^/+/' $archivoHoy > "$ruta/`date +%W`/`date +%Y-%m-%d`/$usuario.txt"
 fi
-
-
-echo "archivo 1"
-cat "$archivo"
-echo "archivo cut"
-cat "$archivoCut"
-echo "archivo 3"
-cat "$archivo3"
-
 done
