e3b4da87ad6c2d1dd101b5076fbec9c1ec29d861
IgnacioVillarreal31 <110312930+IgnacioVillarreal31@users.noreply.github.com>
2023-06-19 21:05:43 -0300

    Add files via upload

--- test.sh
+++ test.sh
@@ -22,6 +22,15 @@ test_archivo_semana_inexistente(){
     assertFalse "Existe la carpeta: " " [ -d $ruta/21/2023-05-27 ] "
 }
 
+test_archivo_(){
+    ruta="/home/ivillarreal/Escritorio/ObligatorioSO-main"
+    archivo=`mktemp`
+    diff -u -b --ignore-all-space --ignore-space-change "$ruta/archivoPruebaTest2" "$ruta/21/2023-05-26/ivillarreal.txt" > $archivo 
+    
+    tamano=$(wc -c < "$archivo")
+    assertTrue "El archivo esta vacio" "[[ $tamano -eq 0 ]]"
+    
+}
 
 
 . /home/ivillarreal/Escritorio/ObligatorioSO-main/shunit2/shunit2
